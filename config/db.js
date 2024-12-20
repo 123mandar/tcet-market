4ba10565acae1fc20d02f56ec12ff658d037d12a
Mandar Dhage <your-email@example.com>
2024-12-21 00:22:50 +0530

    Update Mongoose timeout settings and rebuild client

--- config/db.js
+++ config/db.js
@@ -4,7 +4,10 @@ import colors from "colors";
 const connectDB = async () => {
   try {
     console.log(`Connecting to MongoDB...`.bgYellow.white);
-    const connecting = await mongoose.connect(process.env.MONGO_URL, {});
+    const connecting = await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log(
       `Connection successful with MongoDB at ${connecting.connection.host}`
         .bgGreen.white
